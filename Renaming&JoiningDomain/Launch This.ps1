d522e6b36888563a3ae83f5b5381248e52e844d6
YazidEl7 <94143047+YazidEl7@users.noreply.github.com>
2021-11-18 18:22:52 +0100

    Delete Launch This.ps1

--- Renaming&JoiningDomain/Launch This.ps1
+++
@@ -1,6 +0,0 @@
-# copying the current folder to sys drive where will it be for execution
-Copy-Item -Path "." -Destination "C:\Domainjoin" -Recurse
-# making the directory hidden
-attrib +h "C:\Domainjoin"
-#starting the .bat file which launches the main powershell as admin
-Start-Process -FilePath C:\Domainjoin\Runs_main.bat -Verb Runas
