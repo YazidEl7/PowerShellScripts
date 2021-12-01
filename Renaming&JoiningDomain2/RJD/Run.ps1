02ffd792e03acec14b6d4bbb88cf0b41a33a3676
YazidEl7 <94143047+YazidEl7@users.noreply.github.com>
2021-12-01 22:39:26 +0100

    Delete Renaming&JoiningDomain2 directory

--- Renaming&JoiningDomain2/RJD/Run.ps1
+++
@@ -1,2 +0,0 @@
-Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run'
-Set-ItemProperty -Path . -Name RT -Value 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe "C:\RJD\Runs_Test.bat"'
