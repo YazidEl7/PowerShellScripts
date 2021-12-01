9dbe6f0b8d0e0baec59c46a288686686cba6aeea
YazidEl7 <94143047+YazidEl7@users.noreply.github.com>
2021-12-01 22:25:00 +0100

    Add files via upload

---
+++ Renaming&JoiningDomain2/RJD/domainjoin.ps1
@@ -0,0 +1,13 @@
+﻿#This will be launched after the restart
+# Credentials to be coded later, base64
+$User = "DOMDAMAN\R8198"
+$Pass= ConvertTo-SecureString -String "Plata0Plomo" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $Pass
+#####################################################################################
+add-computer –DomainName DOMDAMAN.INTRA -Credential  $Credential –force
+#####################################################################################
+Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
+Set-ItemProperty -Path . -Name RJDRemoval -Value 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe "Remove-Item -LiteralPath "C:\RJD" -Force -Recurse"'
+
+#####################################################################################
+Restart-Computer -Force
---
+++ Renaming&JoiningDomain2/RJD/main.ps1
@@ -0,0 +1,40 @@
+﻿#######################
+Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run' -Name RT
+
+#When we buy PCs we like to identify it by our own serial numbers generated from our inventory system 
+#not the serial that the vendor alread assigns to the hardware
+#we send to the vendor our own serials
+#to allocate each one to a PC that got already its own serial number from the manufacturer
+#At the end the vendor sends us a file containing two columns (Inventory and Serial)
+#########################################################################################
+#The following code will get the serial number of the PC 
+
+$SerNum = (Get-ComputerInfo).BiosSeralNumber
+
+#The next code will take the inventory number from the db1.csv (that exists too on the current location)
+#that matches the serial number 
+#and store it in a file in the current location
+
+$Lien2="C:\RJD\db1.csv"
+$Inventory = ""
+
+$Records = Import-Csv $Lien2 -Delimiter ";"
+
+ForEach ($record in $records)
+{
+if ( $Record.SERIAL -eq $SerNum ) {
+$Inventory = $Record.INVENTORY 
+Rename-Computer $Inventory         #Renaming PC to the corresponding Inventory value of its serial number
+Break} 
+
+}
+##############################################################################
+#running the earlier copied domainjoin script after computer restart
+
+Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
+Set-ItemProperty -Path . -Name joinDomain -Value 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe "C:\RJD\domainjoin.ps1"'
+# Remove startup file
+#Set-ItemProperty -Path . -Name SBRemoval -Value 'Remove-Item -LiteralPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Runs_Test.bat" -Force'
+
+Restart-Computer -Force
+
