c4459559431ac6174dbe5853c7acd4bdd02392be
YazidEl7 <94143047+YazidEl7@users.noreply.github.com>
2021-12-06 21:43:48 +0100

    Update domainjoin.ps1

--- Renaming&JoiningDomain2/RJD/domainjoin.ps1
+++ Renaming&JoiningDomain2/RJD/domainjoin.ps1
@@ -1,13 +1,16 @@
 ﻿#This will be launched after the restart
 # Credentials to be coded later, base64
-$User = "DOMAIN\Admin"
-$Pass= ConvertTo-SecureString -String "Pass159" -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $Pass
+$User = (Powershell -E IgBEAE8ATwBNAEEASQBOAFwAQQBEAE0ASQBOADIAIgA=)
+$Pass= (Powershell -E IgBQAGEAcwB3AHcAbwByAGQAMQAyADMAIgA=)
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential.($User, ConvertTo-SecureString -String $Pass -AsPlainText -Force)
 #####################################################################################
 add-computer –DomainName DOMDAMAN.INTRA -Credential  $Credential –force
 #####################################################################################
-Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
-Set-ItemProperty -Path . -Name RJDRemoval -Value 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe "Remove-Item -LiteralPath "C:\RJD" -Force -Recurse"'
+#Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
+#Set-ItemProperty -Path . -Name RJDRemoval -Value '$env:systemdrive\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe "Remove-Item -LiteralPath "$env:systemdrive\RJD" -Force -Recurse"'
 
+$D = $Env:Systemdrive
+$P = $D + "\RJD"
+Remove-Item -LiteralPath $P -Force -Recurse 
 #####################################################################################
 Restart-Computer -Force
