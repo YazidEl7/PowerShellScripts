6fbeb5ee1b564fe66cc3156c3c00afbae8f20ff0
YazidEl7 <94143047+YazidEl7@users.noreply.github.com>
2021-12-06 21:30:00 +0100

    Update domainjoin.ps1

--- Renaming&JoiningDomain/domainjoin.ps1
+++ Renaming&JoiningDomain/domainjoin.ps1
@@ -4,7 +4,10 @@ $User = (Powershell -E IgBEAE8ATwBNAEEASQBOAFwAQQBEAE0ASQBOADIAIgA=)
 $Pass= (Powershell -E IgBQAGEAcwB3AHcAbwByAGQAMQAyADMAIgA=)
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential.($User, ConvertTo-SecureString -String $Pass -AsPlainText -Force)
 
+$S = $Env:Systemroot
+$D = $Env:Systemdrive
+$Command = $S + "\System32\WindowsPowerShell\v1.0\powershell.exe " + '"Remove-Item -LiteralPath ' + $D + '\Domainjoin -Force -Recurse"'
 Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce'
-Set-ItemProperty -Path . -Name joinDomain -Value 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe "Remove-Item -LiteralPath "C:\Domainjoin" -Force -Recurse"'
+Set-ItemProperty -Path . -Name joinDomain -Value $Command
 
 add-computer –DomainName DOMAIN.INTRA -Credential  $Credential  -restart –force
