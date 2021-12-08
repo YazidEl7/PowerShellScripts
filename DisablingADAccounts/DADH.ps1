2801e879aebb077c04fbdf31e3f83c8979efea32
YazidEl7 <94143047+YazidEl7@users.noreply.github.com>
2021-12-08 22:24:56 +0100

    Add files via upload

--- DisablingADAccounts/DADH.ps1
+++ DisablingADAccounts/DADH.ps1
@@ -1,21 +1,27 @@
 ﻿Import-Module ActiveDirectory
 $CurrentDate0 = Get-Date -Format "dd-MM-yyyy"
 $Fin0 = [DateTime]::Now.Subtract([TimeSpan]::FromDays(1)) | get-date -format "dd-MM-yyyy"
-$P0 = "./" + $Fin0 + ".csv"
-$ADUsers = Import-csv $P0 -Delimiter ","
+$P0 = "c:\" + $Fin0 + ".csv"
+$ADUsers = Import-csv $P0 -Delimiter ";"
 $newcsv = @()
 
 foreach($user in $ADUsers)
 {
 $matricule = $user.matricule
+#Write-Host $matricule
 $CurrentDate = Get-Date -Format "dd/MM/yyyy"
+#write-host $CurrentDate
 $Debut = Get-Date $user.debutconge -Format "dd/MM/yyyy"
+#write-host $Debut
 $Fin = Get-Date $user.finconge -Format "dd/MM/yyyy"
+#Write-Host $Fin
+#Write-Host "######################################################"
 $Fin1 = [DateTime]::Now.Subtract([TimeSpan]::FromDays(1)) | get-date -format "dd/MM/yyyy"
 
     if ((($Debut -eq $CurrentDate) -and ($Fin -gt $CurrentDate)) -or (($Debut -lt $CurrentDate) -and ($Fin -gt $CurrentDate)) -or (($Debut -lt $CurrentDate) -and ($Fin -eq $CurrentDate)))
     { 
-            Set-ADUser -Identity $user.Matricule -Enabled $false -Verbose
+            Set-ADUser -Identity $user.matricule -Enabled $false -Verbose 
+            write-host "disable"
                  $newcsv += New-Object -TypeName PSObject -Property @{
                          'matricule' = $matricule
                          'debutconge' = $Debut
@@ -24,16 +30,18 @@ $Fin1 = [DateTime]::Now.Subtract([TimeSpan]::FromDays(1)) | get-date -format "dd
     }
     elseif (($Debut -lt $CurrentDate) -and ($Fin1 -eq $Fin)) 
     {
-            Set-ADUser -Identity $user.Matricule -Enabled $true -Verbose
+            Set-ADUser -Identity $user.matricule -Enabled $true -Verbose
+            write-host "enable"
 
     } 
     else 
     {
-
+             Set-ADUser -Identity $user.matricule -Enabled $true -Verbose
+             write-host "enable2"
     }
 
 }
 
-$P = "./" + $CurrentDate0 + ".csv"
-$newcsv | Export-CSV -NoTypeInformation -Path $P -Delimiter "," 
+$P = "C:\" + $CurrentDate0 + ".csv"
+$newcsv | Export-CSV -NoTypeInformation -Path $P -Delimiter ";" 
     
