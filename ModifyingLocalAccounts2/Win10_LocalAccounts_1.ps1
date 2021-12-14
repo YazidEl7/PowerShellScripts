1e1e2299a291b764e72c4ef2592dfef6115b60a2
YazidEl7 <94143047+YazidEl7@users.noreply.github.com>
2021-12-14 22:21:35 +0100

    Add files via upload

---
+++ ModifyingLocalAccounts2/Win10_LocalAccounts_1.ps1
@@ -0,0 +1,93 @@
+﻿Write-Host "############## Modifying win10 local Accounts ######################"
+$Account = (Read-Host -Prompt ' Enter the name of the Local Admin Account ').ToString()
+$Pass0 = (Read-Host -Prompt ' Enter the password you want to use ').ToString()
+$AdminGroup = (Read-Host -Prompt ' Enter the Local Admins group ').ToString()
+Write-Host "######################################################################"
+Write-Host "##  Unavailable computers will be saved to C:\Not_found.txt  ##"
+Write-Host "######################################################################"
+
+$Win10Comp = @( (Get-ADComputer -Filter {OperatingSystem -like '*Windows 10*'} | where {$_.Enabled -eq $True} ).Name )
+$CompCount = ($Win10Comp).Count
+$i = 0
+$Pass= ConvertTo-SecureString -String $Pass0 -AsPlainText -Force
+Foreach ($Computer in $Win10Comp) {
+########
+$i++
+Write-Progress -Activity "Modifying local accounts" -Status "In progress…" -PercentComplete ($i/$CompCount*100)
+########
+$TestConnection = Test-Connection $Computer
+        ########################## Available Computer ####################################################
+        IF ($TestConnection)
+{
+
+         Invoke-Command -ComputerName $Computer -ScriptBlock { 
+         
+         $LocalUsers = (Get-LocalUser).Name
+
+         $LA = $Using:Account
+         echo $LA
+         echo $Using:Pass
+         echo $USing:AdminGroup
+         $Load = Get-ComputerInfo 
+         $OsArch = $Load.OsArchitecture
+         $OsName = $Load.WindowsProductName
+
+         $Check = 0
+         $Check2 = 1
+
+         if ($OsName -ilike "Windows 10 Pro"){
+            if ($OsArch -ilike "64-bit") { 
+                                ######################################################
+                                # Testing the existence of win10 
+                                foreach ($User in $LocalUsers) {
+                                             if ( $User -ilike $LA ) { 
+                                             $Check = 1
+                                             } 
+                                } # 1st foreach
+                                ######################################################
+                                # if the account doesn't exist, we gonna create it 
+                                if ( $Check -eq 0) {
+                                                                               New-LocalUser $LA -Password $Using:Pass
+                                                                               Add-LocalGroupMember -Group $USing:AdminGroup -Member $LA
+                                        
+
+                                        $Check = 1
+                                        $Check2 = 0
+                                } #3rd if
+                                ######################################################
+                                # After Creating the account
+                                if ( $Check -eq 1) {
+                                foreach ($U in $LocalUsers) {
+        
+                                             if ( ($U -ilike $LA) -and ($Check2 -eq 1) ) {
+                                                                             Set-LocalUser -Name $U -Password $Using:Pass -PasswordNeverExpires 1
+                                                                                                         } # 4th if
+          
+                                                else { Disable-LocalUser -Name $U } 
+               
+
+                                } # 2nd foreach 
+                                } # 5th if
+            } #second if
+           } #first if
+          } #invoke
+
+  
+                   
+     
+}
+        ############################### Offline Computer ################################################
+        ElSE
+        {
+            
+             $Offline = "C:\Not_found.txt"
+   
+             if (!(Test-Path -path $Offline))
+             {
+                New-Item $Offline -Type file
+             }
+
+          echo $Computer | Out-File "C:\Not_found.txt" -Append -NoClobber 
+        }
+
+}
---
+++ ModifyingLocalAccounts2/Win10_LocalAccounts_2.ps1
@@ -0,0 +1,109 @@
+﻿Write-Host "############## Modifying win10 local Accounts ######################"
+$Account = (Read-Host -Prompt ' Enter the name of the Local Admin Account ').ToString()
+$Pass = (Read-Host -Prompt ' Enter the password you want to use ').ToString()
+$Group = (Read-Host -Prompt ' Enter the Local Admins group ').ToString()
+Write-Host "######################################################################"
+Write-Host "##  Unavailable computers will be saved to C:\Not_found.txt  ##"
+Write-Host "######################################################################"
+
+$Win10Comp = @( (Get-ADComputer -Filter {OperatingSystem -like '*Windows 10*'} | where {$_.Enabled -eq $True} ).Name )
+$CompCount = ($Win10Comp).Count
+$i = 0
+
+Foreach ($Computer in $Win10Comp) {
+########
+$i++
+Write-Progress -Activity "Modifying local accounts" -Status "In progress…" -PercentComplete ($i/$CompCount*100)
+########
+$TestConnection = Test-Connection $Computer
+        ########################## Available Computer ####################################################
+        IF ($TestConnection)
+{
+        $Check = 0
+        $Check2 = 1
+        #$EnableUser = 512
+        $DisableUser = 2
+
+        # (Get-CimInstance -Class Win32_OperatingSystem -computername BTEST0).OSArchitecture
+        #searching for users
+       ######################################################                                                         
+        $LocalUsers =  @( (Get-CimInstance -ClassName Win32_UserAccount -Filter "LocalAccount = TRUE" -computername $Computer).Name )
+                                ######################################################
+                                # Testing the existence of win10 
+                                foreach ($User in $LocalUsers) {
+                                             if ( $User -ilike "win10-64bit" ) { 
+                                             $Check = 1
+                                             } 
+                                } # 1st foreach
+                                ######################################################
+                                ######################################################
+                                # if the account doesn't exist, we gonna create it 
+                                if ( $Check -eq 0) { 
+                                                                               $objOu = [ADSI]”WinNT://$computer,computer“
+                                                                               $objUser = $objOU.Create(“User“, $Account)
+                                                                               $objUser.setpassword($Pass)
+                                                                               $objUser.SetInfo()
+                                                                               #$user.CommitChanges()
+
+                                                                               # add user to group
+                                                                               $group = [ADSI]"WinNT://$computer/$Group,group" 
+                                                                               $group.add("WinNT://$Account,user")
+                                                                               $group.setinfo()
+
+                                        
+
+                                        $Check = 1
+                                        $Check2 = 0
+                                } 
+                                ######################################################
+         
+         if ( $Check -eq 1) {
+         ForEach($U in $LocalUsers){
+
+             ##############################################
+             if( ($Account -eq $U) -and ($Check2 -eq 1) ) 
+                    {   
+                 $user = [adsi]"WinNT://$computer/$U,user"
+                 $user.SetPassword($Pass)
+                 $user.SetInfo()
+                     }
+             ##############################################
+             else {
+
+                 $user = [adsi]"WinNT://$computer/$U,user"
+                 $user.UserFlags=$DisableUser
+                 $user.SetInfo()
+  
+                    }
+        }
+        }
+}
+        ############################### Offline Computer ################################################
+        ElSE
+        {
+            
+             $Offline = "C:\Not_found.txt"
+   
+             if (!(Test-Path -path $Offline))
+             {
+                New-Item $Offline -Type file
+             }
+
+          echo $Computer | Out-File "C:\Not_found.txt" -Append -NoClobber 
+        }
+
+}
+
+<#   
+  $NewUser = $ADSIComp.Create('User',$Username)
+
+$ADSIComp.Delete('User','TestProx')
+
+
+[ADSI]$Test = "WinNT://$($env:userdomain)/$sam,user"
+If ($test.ADSPath) {
+    Write-Warning "A user with the account name of $sam already exists"
+    #bail out
+    Return
+}
+#>
