df5653b1aa2aca1fe5426cd9cfa7d43bd4160a76
YazidEl7 <94143047+YazidEl7@users.noreply.github.com>
2023-10-26 13:36:30 +0100

    Add files via upload

---
+++ Get-HN_InSubnets/Get-HostNames_InSubnets.ps1
@@ -0,0 +1,38 @@
+﻿
+
+$newcsv = @()
+$P = "c:\HN.csv"
+
+#format "192.168.1"
+$Subnets = Import-csv "C:\Subnets.csv" -Delimiter ";"
+
+
+foreach ($Subnet in $Subnets) {
+echo $Subnets.AR
+    for($i=1; $i -lt 254; $i++){
+
+        $addr = $Subnet.AR + ".$i"
+        echo $addr
+        $HN = (Resolve-DnsName -Name $addr -Type PTR -ErrorAction SilentlyContinue).NameHost
+    
+        if ( ($HN).length -gt 0 ) { 
+
+                 $newcsv += New-Object -TypeName PSObject -Property @{
+                         'Address' = $addr
+                         'Hostname' = $HN    
+                                                                      }
+         }
+     }
+}
+
+
+
+$newcsv | Export-CSV -NoTypeInformation -Path $P -Delimiter ";" 
+
+
+
+
+
+########################
+#Get-NetIPConfiguration
+#Get-DhcpServerInDC
---
+++ Get-HN_InSubnets/README.txt
@@ -0,0 +1,3 @@
+In some environments where you don't have a DHCP maintained in windows server but rather in routers.
+This script helps to get the computer's names, based on subnets configured in routers for a specific 
+location.
